213ce7373b96e2b21e46cce2ac89fdffeb37d1ae
Jose Cuenca <64216815+JCuenca17@users.noreply.github.com>
2023-07-24 21:56:58 -0500

    update GraphLink

--- graphlink/GraphLink.java
+++ graphlink/GraphLink.java
@@ -1,22 +1,43 @@
 package graphlink;
 
-import java.util.ArrayList;
-import java.util.List;
+import listlinked.ListLinked;
 
 public class GraphLink<E> {
 
-    private List<Vertex<E>> verices;
-    private List<Edge<E>> edges;
+    protected ListLinked<Vertex<E>> listVertex;
 
     public GraphLink() {
-        this.verices = new ArrayList<>();
-        this.edges = new ArrayList<>();
+        this.listVertex = new ListLinked<Vertex<E>>();
     }
-    
-    //agregar vertices
-    
-    //agregar aristas
-    
-    //otros
 
-}
+    public void insertVertex(E data) {
+        Vertex<E> v = new Vertex<E>(data);
+        if (this.listVertex.search(v)) {
+            System.out.println("Vertice con " + data + " ya fue insertado");
+        } else {
+            this.listVertex.insertFirst(v);
+        }
+    }
+
+    public void insertEdge(E dataOri, E dataDes) {
+        Vertex<E> vOri = this.listVertex.searchData(new Vertex<E>(dataOri));
+        Vertex<E> vDes = this.listVertex.searchData(new Vertex<E>(dataDes));
+
+        if (vOri == null || vDes == null) {
+            System.out.println(dataOri + " o " + dataDes + "no existen ....");
+        } else {
+            Edge<E> e = new Edge<E>(vDes);
+            if (vOri.listAdj.search(e)) {
+                System.out.println("Arista (" + dataOri + "," + dataDes + ") ya fue insertada ...");
+            } else {
+                vOri.listAdj.insertFirst(e);
+                vDes.listAdj.insertFirst(new Edge<E>(vOri));
+            }
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return this.listVertex.toString();
+    }
