3517a5db1273fb015f8988d29f45b9d075fb95a9
Jose Cuenca <64216815+JCuenca17@users.noreply.github.com>
2023-07-24 19:52:27 -0500

    update vertex

--- graphlink/Vertex.java
+++ graphlink/Vertex.java
@@ -1,5 +1,33 @@
 package graphlink;
 
-public class Vertex {
+public class Vertex<E> {
+
+    private E data;
+    private LinkedList<Edge<E>> edges;
+
+    public Vertex(E data) {
+        this.data = data;
+        this.edges = new LinkedList<>();
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public LinkedList<Edge<E>> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(LinkedList<Edge<E>> edges) {
+        this.edges = edges;
+    }
+
+    public String toString() {
+        return data.toString();
+    }
 
 }
