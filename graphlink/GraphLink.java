68672352a9ef5708be28642bc0bb08ac88c31a0f
Jose Cuenca <64216815+JCuenca17@users.noreply.github.com>
2023-07-24 19:53:06 -0500

    update GraphLink

--- graphlink/GraphLink.java
+++ graphlink/GraphLink.java
@@ -1,5 +1,22 @@
 package graphlink;
 
-public class GraphLink {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphLink<E> {
+
+    private List<Vertex<E>> verices;
+    private List<Edge<E>> edges;
+
+    public GraphLink() {
+        this.verices = new ArrayList<>();
+        this.edges = new ArrayList<>();
+    }
     
+    //agregar vertices
+    
+    //agregar aristas
+    
+    //otros
+
 }
