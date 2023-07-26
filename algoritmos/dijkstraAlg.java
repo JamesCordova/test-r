8bd39fe30f16a0ada4a566830d84b77991dcf039
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-07-25 23:32:47 -0500

    Create dijkstraAlg.java
    algunas bases para el algoritmo dijstra

---
+++ algoritmos/dijkstraAlg.java
@@ -0,0 +1,23 @@
+package Algoritmos;
+
+import Estructuras.GraphLink;
+import Estructuras.Par;
+import Estructuras.Vertex;
+
+public class dijkstraAlg<E> {
+	int [] CosteCamino;
+	GraphLink<Vertex<E>> grafo;
+	Vertex<E> origen;
+	Par<Integer, Integer> [] misPares; 
+	public dijkstraAlg(Vertex<E> origen,GraphLink<Vertex<E>> grafo) {
+		this.grafo = grafo;
+		this.origen = origen;
+		this.misPares = new Par[grafo.getListVertex().getSize()];
+	}
+	private dijkstraAlg() {
+		//prohibido ingresar vacio
+	}
+	public void procesar() {
+		
+	}
+}
