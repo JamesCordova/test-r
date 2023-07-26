57ccd5b76e50a173ac0bf3a4dcff2c05ceb57866
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-07-25 23:36:09 -0500

    agregar PriorityQueueHeap.java

---
+++ estructuras/PriorityQueueHeap.java
@@ -0,0 +1,33 @@
+package Estructuras;
+public class PriorityQueueHeap<T extends Comparable<T>> {
+
+    private Heap<NodeHeap<T>> heap;
+
+    public PriorityQueueHeap() {
+        heap = new Heap<>();
+    }
+
+    public void enqueue(T item, int priority) {
+        NodeHeap<T> element = new NodeHeap<>(item, priority);
+        heap.insert(element);
+    }
+
+    public T dequeue() {
+        NodeHeap<T> element = heap.remove();
+        return element.item;
+    }
+
+    public T front() {
+        NodeHeap<T> element = heap.remove();
+        heap.insert(element);
+        return element.item;
+    }
+
+    public T back() {
+        return heap.remove().item;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+}
