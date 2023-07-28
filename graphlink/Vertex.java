105fd493b1ae3eaeb919426226c5abd0b01e5564
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-07-27 22:49:23 -0500

    agregue un atributo index para usarlo de referencia en arreglos Vertex.java

--- graphlink/Vertex.java
+++ graphlink/Vertex.java
@@ -6,10 +6,12 @@ public class Vertex<E> {
 
     protected E data;
     protected ListLinked<Edge<E>> listAdj;
+    public int indexPosition;
 
     public Vertex(E data) {
         this.data = data;
         this.listAdj = new ListLinked<Edge<E>>();
+        this.indexPosition = -1;
     }
 
     public boolean equals(Object o) {
