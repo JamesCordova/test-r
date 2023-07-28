27055326e10940e1ee75385f5b254ca2da771b71
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-07-27 23:10:46 -0500

    algunos atributos mas y unas pruebas respecto al indice de un verticedijkstraAlg.java

--- algoritmos/dijkstraAlg.java
+++ algoritmos/dijkstraAlg.java
@@ -1,23 +1,36 @@
 package algoritmos;
 
+import java.util.Arrays;
 import Estructuras.GraphLink;
 import Estructuras.Par;
 import Estructuras.Vertex;
 
 public class dijkstraAlg<E> {
-	int [] CosteCamino;
-	GraphLink<Vertex<E>> grafo;
-	Vertex<E> origen;
+	private int [] CosteCamino;
+	private GraphLink<Vertex<E>> grafo;
+	private Vertex<E> origen;
+	private int nVert;
+	private boolean[] know;
+	private final int inf = Integer.MAX_VALUE;
+	
+	
 	Par<Integer, Integer> [] misPares; 
 	public dijkstraAlg(Vertex<E> origen,GraphLink<Vertex<E>> grafo) {
 		this.grafo = grafo;
 		this.origen = origen;
-		this.misPares = new Par[grafo.getListVertex().getSize()];
+		this.nVert = grafo.getListVertex().getSize();
+		this.misPares = new Par[nVert];
+		this.know = new boolean[nVert];
+		this.CosteCamino = new int[nVert];
+		procesar();
 	}
 	private dijkstraAlg() {
 		//prohibido ingresar vacio
 	}
 	public void procesar() {
-		
+        Arrays.fill(CosteCamino, inf);
+//        int aux=grafo.getListVertex().searchIndex(origen.getData());
+//        CosteCamino[aux] = 0;
 	}
 }
+
