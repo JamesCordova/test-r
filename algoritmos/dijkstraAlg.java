457e4935919450f9600e424230b24777daf3ea84
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-07-29 23:34:50 -0500

    Update dijkstraAlg.java

--- algoritmos/dijkstraAlg.java
+++ algoritmos/dijkstraAlg.java
@@ -3,18 +3,19 @@ package algoritmos;
 import java.util.Arrays;
 import Estructuras.GraphLink;
 import Estructuras.Par;
+import Estructuras.PriorityQueueHeap;
 import Estructuras.Vertex;
 
-public class dijkstraAlg<E> {
+public class dijkstraAlg<E extends Comparable<E>> {
 	private int [] CosteCamino;
 	private GraphLink<Vertex<E>> grafo;
 	private Vertex<E> origen;
 	private int nVert;
 	private boolean[] know;
 	private final int inf = Integer.MAX_VALUE;
-	
-	
 	Par<Integer, Integer> [] misPares; 
+	private PriorityQueueHeap<E> myHeap;
+	
 	public dijkstraAlg(Vertex<E> origen,GraphLink<Vertex<E>> grafo) {
 		this.grafo = grafo;
 		this.origen = origen;
@@ -22,6 +23,7 @@ public class dijkstraAlg<E> {
 		this.misPares = new Par[nVert];
 		this.know = new boolean[nVert];
 		this.CosteCamino = new int[nVert];
+		this.myHeap = new PriorityQueueHeap<>();
 		procesar();
 	}
 	private dijkstraAlg() {
@@ -29,8 +31,11 @@ public class dijkstraAlg<E> {
 	}
 	public void procesar() {
         Arrays.fill(CosteCamino, inf);
-//        int aux=grafo.getListVertex().searchIndex(origen.getData());
-//        CosteCamino[aux] = 0;
-	}
+        	int index = grafo.searchVertexIndex(origen);
+//        	int aux=grafo.getListVertex().searchIndex(origen.getData());
+        	CosteCamino[index] = 0;
+        	this.myHeap.enqueue(origen.getData(), 0);
+        }	
 }
+
 
