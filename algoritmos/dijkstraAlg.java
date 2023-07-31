420b56511657ee9222fad4a4894c1690e09f8c00
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-07-31 14:56:14 -0500

    Update dijkstraAlg.java

--- algoritmos/dijkstraAlg.java
+++ algoritmos/dijkstraAlg.java
@@ -1,29 +1,32 @@
 package algoritmos;
 
 import java.util.Arrays;
+
+import Estructuras.Edge;
 import Estructuras.GraphLink;
+import Estructuras.ListLinked;
 import Estructuras.Par;
 import Estructuras.PriorityQueueHeap;
 import Estructuras.Vertex;
 
 public class dijkstraAlg<E extends Comparable<E>> {
 	private int [] CosteCamino;
-	private GraphLink<Vertex<E>> grafo;
+	private GraphLink<E> grafo;
 	private Vertex<E> origen;
 	private int nVert;
 	private boolean[] know;
 	private final int inf = Integer.MAX_VALUE;
 	Par<Integer, Integer> [] misPares; 
-	private PriorityQueueHeap<E> myHeap;
+	private PriorityQueueHeap<Vertex<E>> myHeap;
 	
-	public dijkstraAlg(Vertex<E> origen,GraphLink<Vertex<E>> grafo) {
+	public dijkstraAlg(Vertex<E> origen,GraphLink<E> grafo) {
 		this.grafo = grafo;
 		this.origen = origen;
 		this.nVert = grafo.getListVertex().getSize();
 		this.misPares = new Par[nVert];
 		this.know = new boolean[nVert];
 		this.CosteCamino = new int[nVert];
-		this.myHeap = new PriorityQueueHeap<>();
+		this.myHeap = new PriorityQueueHeap<Vertex<E>>();
 		procesar();
 	}
 	private dijkstraAlg() {
@@ -34,7 +37,23 @@ public class dijkstraAlg<E extends Comparable<E>> {
         	int index = grafo.searchVertexIndex(origen);
 //        	int aux=grafo.getListVertex().searchIndex(origen.getData());
         	CosteCamino[index] = 0;
-        	this.myHeap.enqueue(origen.getData(), 0);
+        	this.myHeap.enqueue(origen, 0);
+        	while(!this.myHeap.isEmpty()) {
+        		Vertex<E> current = this.myHeap.dequeue();
+        		int currentDistance=CosteCamino[grafo.searchVertexIndex(current)];
+        		
+        		for (int i = 0; i < current.getListAdj().getSize(); i++) {
+        			ListLinked<Edge<E>> misVecinos=current.getListAdj();
+					Vertex<E> vecino = misVecinos.getHead().getData().getRefdest();
+					int distP = currentDistance+misVecinos.getHead().getData().getWeight();
+					Edge<E> currentEdge = misVecinos.getHead().getData();
+					if(distP < CosteCamino[grafo.searchVertexIndex(current)]) {
+						CosteCamino[grafo.searchVertexIndex(current)]=distP;
+						myHeap.enqueue(vecino, distP);
+					}
+					
+				}
+        	}
         }	
 }
 
